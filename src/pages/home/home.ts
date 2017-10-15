09b4a87ab95717a726917746aa8d13436d65b07a
Takahiro Suzuki <t.suzuki@techd.jp>
2017-10-15 15:17:04 +0900

    prototype user detail

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -25,7 +25,7 @@ export class HomePage {
 
       type: 'doughnut',
       data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Java", "Angular", "TypeScript", "Kotlin"],
         datasets: [{
           label: '# of Votes',
           data: [12, 19, 3, 5, 2, 3],
